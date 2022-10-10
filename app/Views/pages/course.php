0aa7aa3301596c8eadf02d1fba4d6abe3f939c4a
2017051079_AullyaHananWulandari <aullya.hananwulandari2079@students.unila.ac.id>
2022-10-10 14:17:00 +0700

    up course

---
+++ app/Views/pages/course.php
@@ -0,0 +1,307 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>StudyLab - Free Bootstrap 4 Template by Colorlib</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	
+	<link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+	<link rel="stylesheet" href="css/animate.css">
+	
+	<link rel="stylesheet" href="css/owl.carousel.min.css">
+	<link rel="stylesheet" href="css/owl.theme.default.min.css">
+	<link rel="stylesheet" href="css/magnific-popup.css">
+
+	<link rel="stylesheet" href="css/bootstrap-datepicker.css">
+	<link rel="stylesheet" href="css/jquery.timepicker.css">
+
+	
+	<link rel="stylesheet" href="css/flaticon.css">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+		<div class="container">
+			<a class="navbar-brand" href="index.html"><span>Study</span>Lab</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="oi oi-menu"></span> Menu
+			</button>
+
+			<div class="collapse navbar-collapse" id="ftco-nav">
+				<ul class="navbar-nav ml-auto">
+					<li class="nav-item"><a href="index.html" class="nav-link">Home</a></li>
+					<li class="nav-item"><a href="about.html" class="nav-link">About</a></li>
+					<li class="nav-item active"><a href="course.html" class="nav-link">Course</a></li>
+					<li class="nav-item"><a href="instructor.html" class="nav-link">Instructor</a></li>
+					<li class="nav-item"><a href="blog.html" class="nav-link">Blog</a></li>
+					<li class="nav-item"><a href="contact.html" class="nav-link">Contact</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+	<!-- END nav -->
+	
+	<section class="hero-wrap hero-wrap-2" style="background-image: url('images/bg_2.jpg');">
+		<div class="overlay"></div>
+		<div class="container">
+			<div class="row no-gutters slider-text align-items-end justify-content-center">
+				<div class="col-md-9 ftco-animate pb-5 text-center">
+					<p class="breadcrumbs"><span class="mr-2"><a href="index.html">Home <i class="fa fa-chevron-right"></i></a></span> <span>Course Lists <i class="fa fa-chevron-right"></i></span></p>
+					<h1 class="mb-0 bread">Course Lists</h1>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section class="ftco-section bg-light">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-3 sidebar">
+					<div class="sidebar-box bg-white ftco-animate">
+						<form action="#" class="search-form">
+							<div class="form-group">
+								<span class="icon fa fa-search"></span>
+								<input type="text" class="form-control" placeholder="Search...">
+							</div>
+						</form>
+					</div>
+
+					<div class="sidebar-box bg-white p-4 ftco-animate">
+						<h3 class="heading-sidebar">Kategori Kursus</h3>
+						<form action="#" class="browse-form">
+							<label for="option-category-1"><input type="checkbox" id="option-category-1" name="vehicle" value="" checked>TWK (Tes Wawasan Kebangsaan)</label><br>
+							<label for="option-category-2"><input type="checkbox" id="option-category-2" name="vehicle" value=""> TIU (Tes Intelegensi Umum)</label><br>
+							<label for="option-category-3"><input type="checkbox" id="option-category-3" name="vehicle" value=""> TKP (Tes Karakteristik Pribadi)</label><br>
+						</form>
+					</div>
+
+					<div class="sidebar-box bg-white p-4 ftco-animate">
+						<h3 class="heading-sidebar">Pengajarr</h3>
+						<form action="#" class="browse-form">
+							<label for="option-instructor-1"><input type="checkbox" id="option-instructor-1" name="vehicle" value="" checked> Ronald Jackson</label><br>
+							<label for="option-instructor-2"><input type="checkbox" id="option-instructor-2" name="vehicle" value=""> John Dee</label><br>
+							<label for="option-instructor-3"><input type="checkbox" id="option-instructor-3" name="vehicle" value=""> Nathan Messy</label><br>
+						</form>
+					</div>
+
+					<div class="sidebar-box bg-white p-4 ftco-animate">
+						<h3 class="heading-sidebar">Tipe Kursus</h3>
+						<form action="#" class="browse-form">
+							<label for="option-course-type-1"><input type="checkbox" id="option-course-type-1" name="vehicle" value="" checked>Kumpulan Soal</label><br>
+							<label for="option-course-type-2"><input type="checkbox" id="option-course-type-2" name="vehicle" value=""> Kumpulan Materi</label><br>
+							<label for="option-course-type-3"><input type="checkbox" id="option-course-type-3" name="vehicle" value=""> Video</label><br>
+						</form>
+					</div>
+
+					<!-- <div class="sidebar-box bg-white p-4 ftco-animate">
+						<h3 class="heading-sidebar">Software</h3>
+						<form action="#" class="browse-form">
+							<label for="option-software-1"><input type="checkbox" id="option-software-1" name="vehicle" value="" checked> Adobe Photoshop</label><br>
+							<label for="option-software-2"><input type="checkbox" id="option-software-2" name="vehicle" value=""> Adobe Illustrator</label><br>
+							<label for="option-software-3"><input type="checkbox" id="option-software-3" name="vehicle" value=""> Sketch</label><br>
+							<label for="option-software-4"><input type="checkbox" id="option-software-4" name="vehicle" value=""> WordPress</label><br>
+							<label for="option-software-5"><input type="checkbox" id="option-software-5" name="vehicle" value=""> HTML &amp; CSS</label><br>
+						</form>
+					</div> -->
+				</div>
+				<div class="col-lg-9">
+					<div class="row">
+						<div class="col-md-6 d-flex align-items-stretch ftco-animate">
+							<div class="project-wrap">
+								<a href="#" class="img" style="background-image: url(images/work-1.jpg);">
+									<span class="price">Selengkapnya</span>
+								</a>
+								<div class="text p-4">
+									<h3><a href="#">Soal TWK Tahun 2010</a></h3>
+									<p class="advisor">Pengunggah <span>Ronald Jackson</span></p>
+									<ul class="d-flex justify-content-between">
+										<li><span class="flaticon-shower"></span>2300</li>
+										<li class="price">Free</li>
+									</ul>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-6 d-flex align-items-stretch ftco-animate">
+							<div class="project-wrap">
+								<a href="#" class="img" style="background-image: url(images/work-2.jpg);">
+									<span class="price">Selengkapnya</span>
+								</a>
+								<div class="text p-4">
+									<h3><a href="#">Soal TKP Tahun 2019</a></h3>
+									<p class="advisor">APengunggah <span>Ronald Jackson</span></p>
+									<ul class="d-flex justify-content-between">
+										<li><span class="flaticon-shower"></span>2300</li>
+										<li class="price">Free</li>
+									</ul>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-6 d-flex align-items-stretch ftco-animate">
+							<div class="project-wrap">
+								<a href="#" class="img" style="background-image: url(images/work-3.jpg);">
+									<span class="price">Selengkapnya</span>
+								</a>
+								<div class="text p-4">
+									<h3><a href="#">Soal TIU Tahun 2018</a></h3>
+									<p class="advisor">Pengunggah <span>John Dee</span></p>
+									<ul class="d-flex justify-content-between">
+										<li><span class="flaticon-shower"></span>2300</li>
+										<li class="price">Free</li>
+									</ul>
+								</div>
+							</div>
+						</div>
+
+						<div class="col-md-6 d-flex align-items-stretch ftco-animate">
+							<div class="project-wrap">
+								<a href="#" class="img" style="background-image: url(images/work-4.jpg);">
+									<span class="price">Selengkapnya</span>
+								</a>
+								<div class="text p-4">
+									<h3><a href="#">Materi TWK Course Us!</a></h3>
+									<p class="advisor">Pengunggah <span>John Dee</span></p>
+									<ul class="d-flex justify-content-between">
+										<li><span class="flaticon-shower"></span>2300</li>
+										<li class="price">Free</li>
+									</ul>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-6 d-flex align-items-stretch ftco-animate">
+							<div class="project-wrap">
+								<a href="#" class="img" style="background-image: url(images/work-5.jpg);">
+									<span class="price">Selengkapnya</span>
+								</a>
+								<div class="text p-4">
+									<h3><a href="#">Materi TIU dan TKP Course Us!</a></h3>
+									<p class="advisor">Pengunggah <span>Nathan Messy</span></p>
+									<ul class="d-flex justify-content-between">
+										<li><span class="flaticon-shower"></span>2300</li>
+										<li class="price">Free</li>
+									</ul>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-6 d-flex align-items-stretch ftco-animate">
+							<div class="project-wrap">
+								<a href="#" class="img" style="background-image: url(images/work-6.jpg);">
+									<span class="price">Selengkapnya</span>
+								</a>
+								<div class="text p-4">
+									<h3><a href="#">Video Belajar</a></h3>
+									<p class="advisor">Pengunggah <span>Nathan Messy</span></p>
+									<ul class="d-flex justify-content-between">
+										<li><span class="flaticon-shower"></span>2300</li>
+										<li class="price">Free</li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="row mt-5">
+						<div class="col">
+							<div class="block-27">
+								<ul>
+									<li><a href="#">&lt;</a></li>
+									<li class="active"><span>1</span></li>
+									<li><a href="#">2</a></li>
+									<li><a href="#">3</a></li>
+									<li><a href="#">4</a></li>
+									<li><a href="#">5</a></li>
+									<li><a href="#">&gt;</a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<footer class="ftco-footer ftco-no-pt">
+			<div class="container">
+				<div class="row mb-5">
+					<div class="col-md pt-5">
+						<div class="ftco-footer-widget pt-md-5 mb-4">
+							<h2 class="ftco-heading-2">About</h2>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In id arcu efficitur, maximus turpis sit amet, blandit odio. Donec ultrices consequat leo, at lobortis leo suscipit et. Nullam ac dui quis odio imperdiet ullamcorper ac vel nunc.</p>
+							<ul class="ftco-footer-social list-unstyled float-md-left float-lft">
+								<li class="ftco-animate"><a href="#"><span class="fa fa-twitter"></span></a></li>
+								<li class="ftco-animate"><a href="#"><span class="fa fa-facebook"></span></a></li>
+								<li class="ftco-animate"><a href="#"><span class="fa fa-instagram"></span></a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-md pt-5">
+						<div class="ftco-footer-widget pt-md-5 mb-4 ml-md-5">
+							<h2 class="ftco-heading-2">Help Desk</h2>
+							<ul class="list-unstyled">
+								<!-- <li><a href="#" class="py-2 d-block">Customer Care</a></li>
+								<li><a href="#" class="py-2 d-block">Legal Help</a></li>
+								<li><a href="#" class="py-2 d-block">Services</a></li>
+								<li><a href="#" class="py-2 d-block">Privacy and Policy</a></li>
+								<li><a href="#" class="py-2 d-block">Refund Policy</a></li> -->
+								<li><a href="#" class="py-2 d-block">Call Us</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-md pt-5">
+						<div class="ftco-footer-widget pt-md-5 mb-4">
+							<h2 class="ftco-heading-2">Recent Courses</h2>
+							<ul class="list-unstyled">
+								<li><a href="#" class="py-2 d-block">TWK (Tes Wawasan Kebangsaan)</a></li>
+								<li><a href="#" class="py-2 d-block">TIU (Tes Intelegensi Umum)</a></li>
+								<li><a href="#" class="py-2 d-block">TKP (Tes Kemampuan Pribadi)</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-md pt-5">
+						<div class="ftco-footer-widget pt-md-5 mb-4">
+							<h2 class="ftco-heading-2">Have a Questions?</h2>
+							<div class="block-23 mb-3">
+								<ul>
+									<li><span class="icon fa fa-map-marker"></span><span class="text">203 Fake St. Mountain View, San Francisco, California, USA</span></li>
+									<li><a href="#"><span class="icon fa fa-phone"></span><span class="text">+62 859 0040 8707</span></a></li>
+									<li><a href="#"><span class="icon fa fa-paper-plane"></span><span class="text">info@courseus.com</span></a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-12 text-center">
+
+						<p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+							Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Course Us!</a>
+							<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+						</div>
+					</div>
+				</div>
+			</footer>
+			
+			
+
+			<!-- loader -->
+			<div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+
+
+			<script src="js/jquery.min.js"></script>
+			<script src="js/jquery-migrate-3.0.1.min.js"></script>
+			<script src="js/popper.min.js"></script>
+			<script src="js/bootstrap.min.js"></script>
+			<script src="js/jquery.easing.1.3.js"></script>
+			<script src="js/jquery.waypoints.min.js"></script>
+			<script src="js/jquery.stellar.min.js"></script>
+			<script src="js/owl.carousel.min.js"></script>
+			<script src="js/jquery.magnific-popup.min.js"></script>
+			<script src="js/jquery.animateNumber.min.js"></script>
+			<script src="js/bootstrap-datepicker.js"></script>
+			<script src="js/scrollax.min.js"></script>
+			<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
+			<script src="js/google-map.js"></script>
+			<script src="js/main.js"></script>
+			
+		</body>
+		</html>
