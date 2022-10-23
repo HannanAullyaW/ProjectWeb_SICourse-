a12a9c79cfa56ffe336c5d71f7b23ab5a5e42a1b
2017051079_AullyaHananWulandari <aullya.hananwulandari2079@students.unila.ac.id>
2022-10-23 21:14:14 +0700

    update register gak make header(sementara)

--- app/Views/landingpage/register.php
+++ app/Views/landingpage/register.php
@@ -1,5 +1,4 @@
-<?= $this->extend('landingpage/template'); ?>
-<?= $this->section('content'); ?>
+
 
 <div class="hero-wrap js-fullheight" style="background-image: url('assets/templateweb/images/bannerutama.jpg');">
     <div class="overlay"></div>
@@ -7,7 +6,8 @@
         <div class="row no-gutters slider-text align-items-end justify-content-center">
             <div class="col-md-5 mt-5 order-md-last">
                 <div class="login-wrap p-5 p-md-5  mt-5">
-                    <h3 class="mb-4">Ayo Mulai Belajar!</h3>
+                    <h3 class="mb-4">Mulai Belajar Bersama Kami?</h3>                   
+                    </div>
                     <?php if (session()->getFlashdata('error')) : ?>
                         <div class="alert alert-danger alert-dismissible show fade">
                             <div class="alert-body">
@@ -17,8 +17,12 @@
                             </div>
                         </div>
                     <?php endif; ?>
-                    <form method="POST" action="<?= site_url('auth/prosesMasuk'); ?>"  class="signup-form">
-
+                    <form method="POST"action="<?=site_url('auth/regisProcess')?>" class="signup-form">
+                    
+                        <div class="form-group">
+                            <label class="label" for="name">Nama Lengkap</label>
+                            <input type="text" class="form-control" name="nama" id="nama">
+                        </div>
                         <div class="form-group">
                             <label class="label" for="email">Alamat Email</label>
                             <input type="text" class="form-control" name="email" id="email" required autofocus>
@@ -26,11 +30,32 @@
                         <div class="form-group">
                             <label class="label" for="password">Kata Sandi</label>
                             <input type="password" class="form-control" name="password" id="password" required autofocus>
+                        </div>
+                        <table>
+                            <tr>
+                                <td>
+                                    <div class="justify-content-center ml-5">
+                                        <h6 class="text-center">Sudah Punya Akun? <a href="/masuk">Masuk</a></h6>
+                                    </div>
 
-                            <div class="form-group d-flex justify-content-center mt-4 ">
-                                <button type="submit" class="btn btn-primary submit">Masuk</span></button>
-                            </div>
-                            <h6 class="text-center">Belum Punya Akun? <a href="/register">Daftar</a></h6>
+                                </td>
+                                <td>
+                                    <div class="col-10">
+
+                                    </div>
+                                </td>
+                                <td>
+                                    <div class="form-group d-flex justify-content-end mt-4">
+                                        <button type="submit" class="btn btn-primary submit">Daftar<span class="fa fa-paper-plane"></span>
+                                    </button>
+                                    </div>
+
+                                </td>
+                            </tr>
+
+                        </table>
+
+
                     </form>
 
                 </div>
@@ -40,5 +65,3 @@
     </div>
 </div>
 
-
-<?= $this->endSection(); ?>
