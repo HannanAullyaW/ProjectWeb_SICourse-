b4f862d916a9e7caf1d7f60d858ab905a02d4ae2
Satria Sapta <satriasapta48@gmail.com>
2022-10-23 21:21:15 +0700

    menambahkan fungsi upload pengajar

--- app/Controllers/BimbelController.php
+++ app/Controllers/BimbelController.php
@@ -89,8 +89,55 @@ class BimbelController extends BaseController
     {
         $data = [
             'title' => 'Pengajar',
+            'gambar' =>$this->db->table('pengajar')->get()->getResult()
         ];
         return view('dashboard/pengajar', $data);
+    }
+
+    public function uploadPengajar()
+    {
+        if($this->request->getMethod() ==='post'){
+            $rules = 
+            [
+                
+                'gambar' =>[
+                    'label' => 'Gambar',
+                    'rules' => 'uploaded[gambar]|is_image[gambar]|max_size[gambar,1024]'
+                ],
+                'nama_pengajar' =>[
+                    'label' => 'Nama pengajar',
+                    'rules' => 'required'
+                ],
+                'bidang_pengajar' =>[
+                    'label' => 'Bidang pengajar',
+                    'rules' => 'required'
+                ],
+                'info_pengajar' =>[
+                    'label' => 'Info pengajar',
+                    'rules' => 'required'
+                ],
+            ];
+
+            if($this->validate($rules)){
+                $gambar = $this->request->getFile('gambar');
+                $fileName = time().$gambar->getClientName();
+                $gambar->move('uploads',$fileName);
+
+                $this->db->table('pengajar')->insert([
+                    'nama_pengajar' => $this->request->getPost('nama_pengajar'),
+                    'bidang_pengajar' => $this->request->getPost('bidang_pengajar'),
+                    'info_pengajar' => $this->request->getPost('info_pengajar'),
+                    'gambar' => $fileName
+                ]);
+                
+                return redirect()->back()->with('success', ' Data Berhasil Disimpan');
+            }
+
+            return redirect()->back()->withInput()->with('validation',$this->validator->getErrors());
+        }
+         $data['gambar'] = $this->db->table('pengajar')->get()->getResult(); 
+
+        return view('dashboard/tambahPengajar',$data);
     }
     
 }
