732ee89cbc58293bedd5ed957527360170ae5f7b
ThaliaGemiInnaya <83534462+ThaliaGemiInnaya@users.noreply.github.com>
2022-10-24 10:20:09 +0700

    update ui beranda login

--- app/Views/dashboard/beranda.php
+++ app/Views/dashboard/beranda.php
@@ -6,11 +6,8 @@
 <div class="container">
     <div class="row no-gutters slider-text js-fullheight align-items-center" data-scrollax-parent="true">
         <div class="col-md-7 ftco-animate">
-            <span class="subheading">Selamat Datang di Course Us!</span>
-            <h1 class="mb-4">Belajar lebih mudah dimana pun dan kapan pun dengan Course Us!</h1>
-            <p class="caps">Pahami materi lebih mudah bersama kami, raih mimpi lewat prestasi!</p>
-            <p class="mb-0"><a href="/daftar" class="btn btn-primary">Bergabung Sekarang</a>
-                <a href="/hubungikami" class="btn btn-white">Info Lanjut</a>
+            <span class="subheading">Hi Friend!</span>
+            <h1 class="mb-4">Selamat Datang di Course Us</h1>
             </p>
         </div>
     </div>
