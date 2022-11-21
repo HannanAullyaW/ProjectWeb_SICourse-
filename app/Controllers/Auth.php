72435af1de843b47995f9240efa4851728f0ef5f
RendyLutfiPrabowo <81194811+rendylutfiprabowo@users.noreply.github.com>
2022-11-21 21:40:26 +0700

    edit auth

--- app/Controllers/Auth.php
+++ app/Controllers/Auth.php
@@ -1,15 +1,15 @@
 <?php
 
 namespace App\Controllers;
+
 use App\Models\UserModel;
 
 class Auth extends BaseController
 {
-    
+
 
     public function index()
     {
-        
     }
 
     public function masuk()
@@ -18,7 +18,7 @@ class Auth extends BaseController
         $data = [
             'title' => 'Masuk',
         ];
-        if(session('id')){
+        if (session('id')) {
             return redirect()->to(site_url('/dashboard/beranda'));
         }
 
@@ -32,12 +32,19 @@ class Auth extends BaseController
         $query = $this->db->table('user')->getWhere(['email' => $post['email']]);
         $users = $query->getRow();
         if ($users) {
-            if(password_verify($post['password'],$users->password)){
-                $params=['id'=>$users->id];
+            if (password_verify($post['password'], $users->password)) {
+                $params = ['id' => $users->id];
                 session()->set($params);
+
+                $sessLogin = [
+                    'isLogin' => true,
+                    'username' => $post['username'],
+                    'role' => $post['role']
+                ];
+                $this->session->set($sessLogin);
+
                 return redirect()->to(site_url('dashboard/beranda'));
-                
-            } else{
+            } else {
                 return redirect()->back()->with('error', 'Password Salah!');
             }
         } else {
@@ -46,35 +53,43 @@ class Auth extends BaseController
     }
 
     public function logout()
-	{
-		session()->destroy();
-		return redirect()->to(base_url('/'));
-	}
-    
-    public function register(){
+    {
+        session()->destroy();
+        return redirect()->to(base_url('/'));
+    }
+
+    public function register()
+    {
         $data = [
             'title' => 'Daftar',
         ];
-        if(session('id')){
+        if (session('id')) {
             return redirect()->to(site_url('/dashboard/beranda'));
         }
-        return view('landingpage/daftar',$data);
+        return view('landingpage/daftar', $data);
     }
-    public function regisProcess(){
+    public function regisProcess()
+    {
+        // $tangkap = $this->request->getPost();
+        // $this->validation->run($tangkap, 'register');   
+        // $errors = $this->validation->getErrors();
+
+        // if($errors){
+        //     session()->setFlashdata('error', $errors);
+        //     return redirect()->to('/auth/register');
+        // }
+
         $userModel = model(UserModel::class); // Sesuai Dokumentasi CI4 https://codeigniter.com/user_guide/tutorial/create_news_items.html
         $data = [
-            
-            'nama' => $this -> request -> getPost('nama'),
-            'email' => $this -> request -> getPost('email'),
+
+            'nama' => $this->request->getPost('nama'),
+            'email' => $this->request->getPost('email'),
             'password' => password_hash($this->request->getVar('password'), PASSWORD_BCRYPT),
 
         ];
         $userModel->save($data);
         $session = session();
-        session()->setFlashdata('message','selamat registrasi berhasil!');
+        session()->setFlashdata('message', 'selamat registrasi berhasil!');
         return redirect()->to('masuk');
-
-
     }
-
 }
